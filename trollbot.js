1f357e06f2ca4238c956f9d495cc747fcf5eda7e
W.M. <Wingman4l7@gmail.com>
2013-07-30 00:02:48 +1000

    added callbacks, respond-in-kind

--- trollbot.js
+++ trollbot.js
@@ -4,19 +4,24 @@
 var irc = require('irc'); // npm install irc : https://github.com/martynsmith/node-irc
   
 var config = {
-		channel : ['#infested_channel'],
+		channels : ['#trolls', '#more_trolls'],
 		server  : 'chat.freenode.net',
 		name    : 'trollbot',
 		owner   : 'your_user_name',
 		killcmd : 'secret_phrase',
-		trolls  : ['this_user_is_a_troll', 'so_is_this_user'],
+		trolls  : ['troll_user', 'another_troll'],
 		phrases : ['HERP DERP', 'O RLY', 'NO U', 'Y U NO', 'u mad bro']
 };
 
-// connect to the IRC channel
+// connect to the IRC channel(s)
 try {
-	var bot = new irc.Client(config.server, config.name, {channels: config.channel});
-	console.log('joined %s', config.channel);
+	var bot = new irc.Client(config.server, config.name, 
+								{channels: config.channels, 
+								autoConnect: false}
+							);
+	bot.connect(function() {
+		console.log('joined %s', config.channel);
+	});
 }
 catch (err) {
 	console.log('error: %s, tried to create %s', err.code, err.path); 
@@ -30,22 +35,21 @@ bot.addListener('error', function(message) {
 
 // listen for chat messages
 bot.addListener('message', function(nick, to, text, message) {
-	// is troll on list?
+	// is troll in list?
 	if (config.trolls.indexOf(nick) > -1) {
 		// randomly pick phrase to say to troll
 		var pick = config.phrases[Math.floor(Math.random() * config.phrases.length)];
-		bot.say(config.channel[0], pick);
+		if (to == config.name) {  // private message
+            bot.say(nick, pick); //  respond in kind
+        }
+        else {
+            bot.say(to, pick); // respond publicly in channel
+        }    
 	}
-	
 	// allows bot owner to issue a remote kill command
 	if (nick == config.owner && text == config.killcmd) {
-		exitIRC(); 
+		bot.disconnect( function() {
+			console.log('left %s', config.channels);
+		}); 
 	}
 });
-
-// executes these actions after receiving a successful kill command
-function exitIRC() {
-	bot.part(config.channel);
-	console.log("left channel %s", config.channel);
-	process.exit();  // remove this if you want to add code to revive the bot remotely
-}
